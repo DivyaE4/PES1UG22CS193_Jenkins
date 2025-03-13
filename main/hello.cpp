c903a995e341f66e47bf806ca6f2d2ec441dd1cd
DivyaE4 <117757807+DivyaE4@users.noreply.github.com>
2025-03-13 16:12:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Continuous Build - Task 4"
+  cout << "Continuous Build - Task 4 work"
   return 0;
 }
